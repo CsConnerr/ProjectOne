fd50804a1d404dcf1bb09c66ba12d0f1de767ebc
Conner <clramirezxr1999@gmail.com>
2022-01-31 23:45:13 -0800

    My Name

--- src/Project.java
+++ src/Project.java
@@ -1,5 +1,5 @@
 public class Project {
     public static void main(String[] args) throws Exception {
-        System.out.println(" \n Hello, World! \n ");
+        System.out.println(" \n Hello, World! My name is Conner. \n ");
     }
 }
